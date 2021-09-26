a55a11d28a9ce2ad678e895441fa3f10d4b492c9
Michal Bednarek <michalbedn1@gmail.com>
2021-09-26 14:34:01 +0100

    Update Database.java

--- Database.java
+++ Database.java
@@ -28,6 +28,7 @@ public class Database {
 		return userIDs;
 	}
 	
+	
 	// Returns user IDs of users in the MessageGroup "groupid"
 	public ArrayList<String> getGroupUserIDs(int groupid){
 		ArrayList<String> userIDs = new ArrayList<String>();
@@ -43,6 +44,7 @@ public class Database {
 		 }
 		return userIDs;
 	}
+	
 	
 	// List of <displayname, message text> for all messages in the group
 	public ArrayList<ArrayList<String>> getGroupMessages(String groupName){
@@ -66,33 +68,181 @@ public class Database {
 	}
 
 	
-	//public ArrayList<ArrayList<String>> getAllMessagesByUser(int usid){
-		
-	//}
+	// Displayname, groupname and messagetext for all messages posted to all groups by user
+	public ArrayList<ArrayList<String>> getAllMessagesByUser(int usid){
+		ArrayList<ArrayList<String>> messagesList = new ArrayList<ArrayList<String>>();
+
+		try {
+			PreparedStatement query = con.prepareStatement("SELECT displayname, groupname, messagetext FROM messageuser NATURAL JOIN message NATURAL JOIN messagegroup WHERE messageuser.usid = ?");
+			query.setInt(1, usid);
+			
+			ResultSet rs = query.executeQuery();
+			while (rs.next()) {
+				ArrayList<String> r = new ArrayList<String>();
+				r.add(rs.getObject("displayname").toString());
+				r.add(rs.getObject("groupname").toString());
+				r.add(rs.getObject("messagetext").toString());
+				messagesList.add(r);
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return messagesList;
+	}	
 	
-	//public ArrayList<String> getMostActiveUsers(){
-		
-	//}
 	
-	//public ArrayList<String> getInactiveUsers(){
-		
-	//}
-	
+	// Adds a new user with a display name and phone number
 	public void addUser(String displayName, int phoneNumber) {
+		int nextID = getNextUserID();
 		
+		try {
+			PreparedStatement query = con.prepareStatement("INSERT INTO messageuser (values(?,?,?))");
+			
+			query.setInt(1,nextID);
+			query.setInt(2,phoneNumber);
+			query.setString(3,displayName);
+			
+			query.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
 	}
 	
-	public void addGroup(String groupName) {
+	
+	// Adds a user to a specified group
+	public void addUserToGroup(int usid, String groupname) {
+		try {
+			PreparedStatement stmt = con.prepareStatement("INSERT INTO membership values ((SELECT groupid FROM messagegroup WHERE groupname = ?),?)");
 		
+			stmt.setString(1,groupname);
+			stmt.setInt(2,usid);
+			
+			stmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 	
+	// Adds a new group with name groupName
+	public void addGroup(String groupName) {		
+		int newid = getNextGroupID();
+		
+		try {	
+			PreparedStatement stmt = con.prepareStatement("insert into messagegroup values(?,?)");
+			
+			stmt.setInt(1, newid);
+			stmt.setString(2, groupName);
+		
+			stmt.executeUpdate();	
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	// Adds a new message by user to group with the context text
 	public void addMessage(int usid, int groupid, String text) {
+		int nextid = getNextMessageID();
 		
+		try {			
+			PreparedStatement stmt = con.prepareStatement("INSERT INTO message values(?,?,?,?)");
+			
+			stmt.setInt(1, nextid);
+			stmt.setInt(2, usid);
+			stmt.setInt(3, groupid);
+			stmt.setString(4, text);
+			
+			stmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+
+	// Returns next available userID for User 
+	private int getNextUserID() {
+		String query = "SELECT max(usid) FROM messageuser";
+		int maxID = 0;
+		
+		try {
+			PreparedStatement stmt = con.prepareStatement(query);
+			ResultSet rs = stmt.executeQuery();
+			while (rs.next()) {
+				maxID = rs.getInt(1);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		if (maxID < 0) {
+			return maxID;
+		}
+		return maxID + 1;
+	}
+
+	
+	// Returns next available message ID for message
+	private int getNextMessageID() {
+		String query = "SELECT max(messageid) FROM message";
+		int maxid = 0;
+		
+		try {
+			PreparedStatement stmt = con.prepareStatement(query);	
+			ResultSet rs = stmt.executeQuery(query);
+			while (rs.next()) {
+				maxid = rs.getInt(1);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		if (maxid < 0) {
+			return maxid;
+		}
+		return maxid + 1;
 	}
 	
 	
+	// Returns next available group ID for group
+	private int getNextGroupID() {
+		String query = "SELECT max(groupid) FROM messagegroup";
+		int maxid = 0;
+		
+		try {
+			PreparedStatement stmt = con.prepareStatement(query);
+			ResultSet rs = stmt.executeQuery(query);
+			while (rs.next()) {
+				maxid = rs.getInt(1);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		if (maxid < 0) {
+			return maxid;
+		}
+		return maxid + 1;
+	}
+
+
 	
-	
+	// Prints an ArrayList of ArrayList<String>
+	private static void print2(ArrayList<ArrayList<String>> list) {
+		for (ArrayList<String> s: list) {
+			print1(s);
+			System.out.println();
+		}
+	}
+		
+	// Prints an ArrayList
+	private static void print1(ArrayList<String> list) {
+		for (String s: list) {
+			System.out.print(s + " ");
+		}
+	}
+
 	
 	
 	// Executes a query and returns a ResultSet
